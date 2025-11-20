14bf0a9f124033710b4bf372d3f709fb1adb3e8d
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-21 01:11:49 +0600

    Context API

---
+++ Context/src/Components/Section.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { LevelContext } from "../Context/LevelContext";
+export default function Section({ children }) {
+  const level = useContext(LevelContext)
+  return (
+    <section className="section">
+      <LevelContext.Provider value={level + 1}>
+        {children}
+      </LevelContext.Provider>
+    </section>
+  );
+}
