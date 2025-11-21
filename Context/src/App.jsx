eb8e03a95bebcf4e8042d557a6ef40e3bb2c710d
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-21 16:55:26 +0600

    Context

--- Context/src/App.jsx
+++ Context/src/App.jsx
@@ -1,25 +1,75 @@
 import Section from './Components/Section';
 import Heading from './Components/Heading';
 
-export default function Page() {
+// export default function Page() {
+//   return (
+//     <Section>
+//       <Heading>Title</Heading>
+//       <Section>
+//         <Heading>Heading</Heading>
+//         <Heading>Heading</Heading>
+//         <Heading>Heading</Heading>
+//         <Section>
+//           <Heading>Sub-heading</Heading>
+//           <Heading>Sub-heading</Heading>
+//           <Heading>Sub-heading</Heading>
+//           <Section>
+//             <Heading>Sub-sub-heading</Heading>
+//             <Heading>Sub-sub-heading</Heading>
+//             <Heading>Sub-sub-heading</Heading>
+//           </Section>
+//         </Section>
+//       </Section>
+//     </Section>
+//   );
+// }
+
+
+export default function ProfilePage() {
+  return (
+    <Section isFancy={true}>
+      <Heading >My Profile</Heading>
+      <Post
+        title="Hello traveller!"
+        body="Read about my adventures."
+      />
+      <AllPosts />
+    </Section>
+  );
+}
+
+function AllPosts() {
+  return (
+    <Section isFancy={true} >
+      <Heading>Posts</Heading>
+      <RecentPosts />
+    </Section>
+  );
+}
+
+function RecentPosts() {
   return (
     <Section>
-      <Heading>Title</Heading>
-      <Section>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Section>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Section>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-          </Section>
-        </Section>
-      </Section>
+      <Heading>Recent Posts</Heading>
+      <Post
+        title="Flavors of Lisbon"
+        body="...those pastéis de nata!"
+      />
+      <Post
+        title="Buenos Aires in the rhythm of tango"
+        body="I loved it!"
+      />
+    </Section>
+  );
+}
+
+function Post({ title, body }) {
+  return (
+    <Section isFancy={true}>
+      <Heading>
+        {title}
+      </Heading>
+      <p><i>{body}</i></p>
     </Section>
   );
 }
--- Context/src/Components/Section.jsx
+++ Context/src/Components/Section.jsx
@@ -1,12 +1,26 @@
 import { useContext } from "react";
 import { LevelContext } from "../Context/LevelContext";
-export default function Section({ children }) {
-  const level = useContext(LevelContext)
+// export default function Section({ children }) {
+//   const level = useContext(LevelContext)
+//   return (
+//     <section className="section">
+//       <LevelContext.Provider value={level + 1}>
+//         {children}
+//       </LevelContext.Provider>
+//     </section>
+//   );
+// }
+
+export default function Section({ children, isFancy }) {
+  const level = useContext(LevelContext);
   return (
-    <section className="section">
-      <LevelContext.Provider value={level + 1}>
+    <section className={
+      'section ' +
+      (isFancy ? 'fancy' : '')
+    }>
+      <LevelContext value={level + 1}>
         {children}
-      </LevelContext.Provider>
+      </LevelContext>
     </section>
   );
 }
