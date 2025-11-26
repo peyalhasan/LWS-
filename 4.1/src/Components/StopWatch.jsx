bf6f33369f270b2c72f0c41d8090e99a6059bf66
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-27 02:23:45 +0600

    useRef concepts

--- 4.1/src/Components/StopWatch.jsx
+++ 4.1/src/Components/StopWatch.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const StopWatch = () => {
- const [startTime, setStartTime] = useState(null)
- const [now, setNow] = useState(null)
+    const [start, setStart] = useState(null);
+    const [now , setNow] = useState(null);
+    let intervalReferance = useRef(null);
     
     function handleStart(){
-        setStartTime(Date.now());
-        setNow(Date.now())
+        setStart(Date.now());
+        setNow(Date.now());
+        
+        intervalReferance.current = setInterval(()=>{
+            setStart(Date.now())
+        }, 10)
     }
-    setInterval(()=>{
-        setNow(Date.now())
-    }, 10)
-    let secondsPassed = 0;
-    if(startTime != null && now != null){
-        secondsPassed = (now - startTime) / 1000;
+    function handleStop(){
+        clearInterval(intervalReferance.current)
+    }
+    let counted = 0;
+    if(start != null & now != null){
+        counted= (start - now) / 1000;
     }
     return (
         <div>
-            <h1>Time passed: {secondsPassed.toFixed(3)} </h1>
-            <button onClick={handleStart}>Start</button>
+            <h1>Time passed: {counted.toFixed(3)} </h1>
+            <button onClick={handleStart} > Start </button>
+            <button onClick={handleStop}>Stop</button>
         </div>
     );
 };
