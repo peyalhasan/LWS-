550f18bc94e086aeb15ef782ea1a348396a95422
Peyal Hasan <peyalhasan143@gmail.com>
2025-12-04 22:50:15 +0600

    Hooks

--- 4.23/src/Components/Form.jsx
+++ 4.23/src/Components/Form.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react';
 import useFormInput from '../Hooks/useFormInput';
 
 export default function Form() {
-  const firstNameProps = useFormInput('Mary')
-  const lastNameProps = useFormInput('Poppins')
-  return (
-    <>
-      <label>
-        First name:
-        <input {...firstNameProps} />
-      </label>
-      <br />
-      <br />
-      <label>
-        Last name:
-        <input {...lastNameProps} />
-      </label>
-      <p><b>Good morning, {firstNameProps.value} {lastNameProps.value}.</b></p>
-    </>
-  );
+    const firstNameProps = useFormInput('Mary')
+    const lastNameProps = useFormInput('Poppins')
+    return (
+        <>
+            <label>
+                First name:
+                <input {...firstNameProps} />
+            </label>
+            <br />
+            <br />
+            <label>
+                Last name:
+                <input {...lastNameProps} />
+            </label>
+            <p><b>Good morning, {firstNameProps.value} {lastNameProps.value}.</b></p>
+        </>
+    );
 }
